4667668ccbdd13b042c703c9e7300821bc8a0612
diasmatheus05 <diasmatheus05@gmail.com>
2023-02-28 10:55:13 -0300

    first commit: creating repository

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("App", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/Hello Word/i);
+  expect(text).toBeInTheDocument();
 });
